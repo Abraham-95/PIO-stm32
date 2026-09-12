7bf9535eaa08cb602216e64b86bec4133e13c90b
Abraham-95 <smileless00@gmail.com>
2026-09-13 02:30:07 +0700

    first succes

--- include/Utility.h
+++ include/Utility.h
@@ -1,26 +1,32 @@
 #pragma once
 #include "stm32f4xx_hal.h"
-#include <cstdint>
-#include <cstdarg>
 
-void USB_Print(const char* str);
-void USB_Printf(const char* fmt, ...);
+#ifdef __cplusplus
+  #include <cstdint>
+  #include <cstdarg>
+#else
+  #include <stdint.h>
+  #include <stdarg.h>
+#endif
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+void USB_Print(const char* str);
+void USB_Printf(const char* fmt, ...);
 // millis() and micros()
 void DWT_Init(void);
 uint32_t micros(void);
 uint32_t millis(void);
-// main.cpp
+// dipanggil dari main.c
 void setup(void);
 void loop(void);
 void uartByteReceived(void);
-
 #ifdef __cplusplus
 }
 #endif
+
+#ifdef __cplusplus
 // Serial3
 class UART {
 public:
@@ -38,3 +44,4 @@ private:
   volatile uint16_t tail = 0;
   uint8_t rxByte;
 };
+#endif
